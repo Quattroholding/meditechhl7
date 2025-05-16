3ffa6c748941ff084c2734aedaf296de9ebf9ff2
Rafael Gasperi <rafgasperi@yahoo.com>
2025-05-16 16:25:33 -0500

    bug fix

--- resources/views/livewire/item-transfer.blade.php
+++ resources/views/livewire/item-transfer.blade.php
@@ -6,13 +6,18 @@
     <div class="flex" x-data>
         <!-- Tabla de disponibles -->
         <div class="w-10/12" style="border: 3px solid #2E37A4;padding: 20px;">
-            <h2 class="text-center">Campos Disponibles</h2>
+            <h2 class="text-center">{{__('Secciones Disponibles')}}</h2>
             <ul class="list-group" id="basic-list-group">
                 @foreach($availableItems as $item)
-                    <li class="list-group-item draggable text-end">
-                        <div class="text-left">{{ $item['section'] }} | {{ $item['label'] }}</div>
+                    <li class="list-group-item">
+                        <div class="text-end" style="display: inline-block"
+                             data-bs-toggle="tooltip"
+                             data-bs-html="true"
+                             data-bs-original-title="<ul> @foreach($item['livewire_component_fields'] as $field) <li>{{$field['name']}}</li> @endforeach </ul>">{{ $item['name_esp'] }}</div>
+                        <button wire:click="moveToSelected({{ $item['id'] }})" type="button"  title="hacer click para mover a la derecha">
+                            <i class="fa fa-arrow-right" class="text-end"></i>
+                        </button>
 
-                        <button wire:click="moveToSelected({{ $item['id'] }})" type="button"  title="hacer click para mover a la derecha"><i class="fa fa-arrow-right"></i> </button>
                     </li>
                 @endforeach
             </ul>
@@ -20,12 +25,18 @@
         <div class="w-10/12 top-2"><i class="fa fa-exchange fa-4x" style="margin:0.5em;"></i> </div>
         <!-- Tabla de seleccionados -->
         <div class="w-10/12" style="border: 3px solid #2E37A4;padding: 20px;">
-            <h2 class="text-center">Campos Seleccionados</h2>
-            <ul id="sortable-list" class="space-y-2">
+            <h2 class="text-center">{{__('Secciones Seleccionados')}}</h2>
+            <ul  class="list-group" id="basic-list-group">
                 @foreach($selectedItems as $item)
-                    <li class="border p-2 flex justify-between text-left" data-id="{{ $item['id'] }}">
-                        <button class="" wire:click="moveToAvailable({{ $item['id'] }})" type="button" title="hacer click para mover a la izquierda"><i class="fa fa-arrow-left"></i> </button>
-                        <div class="text-left">{{ $item['section'] }} | {{ $item['label'] }}</div>
+                    <li class="list-group-item draggable ">
+                        <div class="text-left" style="display: inline-block"
+                             data-bs-toggle="tooltip"
+                             data-bs-html="true"
+                             data-bs-original-title="<ul> @foreach($item['livewire_component_fields'] as $field) <li>{{$field['name']}}</li> @endforeach </ul>">{{ $item['name_esp'] }}</div>
+                        <button  wire:click="moveToAvailable({{ $item['id'] }})" type="button" title="hacer click para mover a la izquierda">
+                            <i class="fa fa-arrow-left"></i>
+
+                        </button>
                     </li>
                 @endforeach
             </ul>
