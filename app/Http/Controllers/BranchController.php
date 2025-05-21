8168de29a37455b20b0a5fb2d7be5d5f34a1b684
ag-tenorio <atenorio@hcaglobal.com>
2025-05-21 08:46:16 -0500

    create branches done

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Branch;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
@@ -12,7 +13,31 @@ class BranchController extends Controller
     }
 
     public function store(Request $request){
+        //dd($request->all());
+        $validated = $request->validate([
+            "client_id" => "required",
+            "name" => "required",
+            "phone" => "required",
+            "full_phone" => "required",
+            "address" => "required",
+            "type" => "required"
+        ]);
 
+        $model = new Branch();
+        $model->client_id = $request->client_id;
+        $model->name = $request->name;
+        $model->phone = $request->full_phone;
+        $model->address = $request->address;
+        $model->type = $request->type;
+        $model->active = 1;
+
+        if($model->save()){
+            $request->session()->flash('message.success','Sucursal registrada con éxito.');
+        }else{
+            $request->session()->flash('message.success','Hubo un error y no se pudo crear la sucursal.');
+        }
+
+        return redirect(route('client.index'));
     }
 
     public function edit($id){
