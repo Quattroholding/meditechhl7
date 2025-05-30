8a02922ae8ff550ddfd7900646616eeeaab2f152
Rafael Gasperi <rafgasperi@yahoo.com>
2025-05-30 16:35:17 -0500

    bug fix

--- resources/views/livewire/patient/create.blade.php
+++ resources/views/livewire/patient/create.blade.php
@@ -2,18 +2,7 @@
     @section('scripts')
         <script src="{{ URL::asset('/assets/js/bootstrap-datetimepicker.min.js') }}"></script>
     @stop
-    @if (session()->has('message'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('message') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif
-    @if (session()->has('error'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('message') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif
+    @include('partials.message')
     <form wire:submit="savePatient">
         @csrf
         <div class="row">
