e90b0e2791338e9f19d96913c8293687b7012216
Rafael Gasperi <rafgasperi@yahoo.com>
2025-07-10 16:28:08 -0500

    bug fix

--- resources/views/livewire/doctor/signature-manager.blade.php
+++ resources/views/livewire/doctor/signature-manager.blade.php
@@ -56,6 +56,7 @@
                         </div>
                     @endif
 
+
                     <!-- Subir nueva firma -->
                     <div class="upload-section">
                         <div class="mb-3">
