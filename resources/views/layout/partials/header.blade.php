82489273cf221cf76e5e8e08356314def60cd16e
Rafael Gasperi <rafgasperi@yahoo.com>
2025-07-30 11:34:32 -0500

    bug fix

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,7 +1,7 @@
 <div class="header">
     <div class="header-left">
         <a href="{{ route('dash') }}" class="logo">
-           <img src="{{url('images/logo1.png')}}" alt=""  style="width: 80%;">
+           <img src="{{url('images/logo1.png')}}" alt="">
         </a>
     </div>
     <a id="toggle_btn" href="javascript:void(0);"><i class="fa fa-bars"></i></a>
