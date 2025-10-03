45bb94cbf5f0127f2933b2c3b68e82a7f1bf0762
Rafael Gasperi <rafgasperi@yahoo.com>
2025-10-03 09:34:49 -0500

    bug fix

--- app/Http/Controllers/ConsultationController.php
+++ app/Http/Controllers/ConsultationController.php
@@ -86,6 +86,13 @@ class ConsultationController extends Controller
                 ->where('status', 'billable')
                 ->get();
 
+            // Log charge items for debugging
+            \Log::info('ChargeItems for encounter', [
+                'encounter_id' => $encounter->id,
+                'billable_count' => $chargeItems->count(),
+                'all_charge_items' => ChargeItem::where('encounter_id', $encounter->id)->get(['id', 'status', 'service_description'])->toArray(),
+            ]);
+
             $invoice = null;
 
             if ($chargeItems->count() > 0) {
