4b8ae5f4afab397980595c4dbbb92888ada3473a
Rafael Gasperi <rafgasperi@yahoo.com>
2025-11-14 08:31:37 -0500

    bug fix

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Política de Privacidad - {{ config('app.name') }}</title>
+    <link rel="icon" href="{{url('images/favicon.ico')}}" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         body {
