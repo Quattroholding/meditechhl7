094767a9545156d3e461139760ac890daa0557a4
Rafael Gasperi <rafgasperi@yahoo.com>
2025-11-26 08:04:43 -0500

    bug fix

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (behind Nginx, Cloudflare, load balancers, etc.)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Headers that should be used to detect the proxy-related data.
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_FORWARDED;
+}
+
