36bd1874e1b445ae1d0f47fd06f9db48569394f7
Rafael Gasperi <rafgasperi@yahoo.com>
2025-12-10 11:16:51 -0500

    bug fix

--- database/seeders/UserAdminSeeder.php
+++ database/seeders/UserAdminSeeder.php
@@ -21,6 +21,7 @@ class UserAdminSeeder extends Seeder
             'last_name' => 'Del Sistema',
             'email' => 'rgasperi@smartcarebilling.com',
             'password' => 'Prueba.1',
+
         ]);
 
         $admin->assignRole('admin');
