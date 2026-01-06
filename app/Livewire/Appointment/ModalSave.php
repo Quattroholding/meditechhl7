56d8cf001c9c802e69e5231c495a639dba53e738
Rafael Gasperi <rafgasperi@yahoo.com>
2026-01-06 15:41:40 -0500

    bug fix

--- app/Livewire/Appointment/ModalSave.php
+++ app/Livewire/Appointment/ModalSave.php
@@ -600,7 +600,7 @@ class ModalSave extends Component
             $this->notes = $this->appointment->comment;
             $this->canEdit = auth()->user()->can('edit', $this->appointment);
             $this->showModal = true;
-            $this->dispatch('cita-message', message: 'Cita actualizada exitosamente.');
+            //$this->dispatch('cita-message', message: 'Cita actualizada exitosamente.');
         }
     }
 
