5663752aa9d0bff7b9685ac59817d76fba67e676
Rafael Gasperi <rafgasperi@yahoo.com>
2026-01-07 07:46:06 -0500

    bug fix

--- app/Livewire/Appointment/Calendar.php
+++ app/Livewire/Appointment/Calendar.php
@@ -213,7 +213,7 @@ class Calendar extends Component
 
         $this->appointments = $query->orderBy('start')->get()->toArray();
 
-        if($this->currentView == 'weekly' && auth()->user()->id==1) dd($this->appointments);
+        //if($this->currentView == 'weekly' && auth()->user()->id==1) dd($this->appointments);
     }
 
     #[On('loadStats')]
