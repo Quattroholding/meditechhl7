d14b2f1ded39d91d25d508ac3dfd6ee23f4d8c24
Rafael Gasperi <rafgasperi@yahoo.com>
2026-03-11 15:17:48 -0500

    bug fix

--- app/Models/Scopes/PractitionerScope.php
+++ app/Models/Scopes/PractitionerScope.php
@@ -13,7 +13,9 @@ class PractitionerScope implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {
-        if (auth()->user() && (auth()->user()->hasRole('admin client') or auth()->user()->hasRole('recepcionista'))) {  // el doctor solo ve los clientes que tiene asociados
+        if (auth()->user() && (auth()->user()->hasRole('admin client')
+                or auth()->user()->hasRole('recepcionista')
+                or auth()->user()->hasRole('asistente medico'))) {  // el doctor solo ve los clientes que tiene asociados
             $builder->whereHas('user', function ($q) {
                 $q->whereHas('clients', function ($q2) {
                     $q2->whereIn('user_clients.client_id', auth()->user()->clients()->pluck('client_id'));
