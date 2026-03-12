842a5b4bf94780837839bbe5445a032d10fe3ffd
ag-tenorio <atenorio@hcaglobal.com>
2026-03-12 14:41:33 -0500

    bugs fixed in help section

--- resources/views/help/support.blade.php
+++ resources/views/help/support.blade.php
@@ -162,7 +162,7 @@
                         <i class="fab fa-whatsapp contact-icon" style="color: #25d366;"></i>
                         <h4>WhatsApp</h4>
                         <p>Atención rápida para dudas puntuales y emergencias menores.</p>
-                        <a href="#" class="btn btn-success btn-sm">Chatea ahora</a>
+                        <a href="https://wa.me/5078316174" class="btn btn-success btn-sm">Chatea ahora</a>
                     </div>
                 </div>
                 <div class="col-md-4">
@@ -178,7 +178,7 @@
                         <i class="fas fa-phone-alt contact-icon" style="color: #34a853;"></i>
                         <h4>Llamada Técnica</h4>
                         <p>Asistencia directa y guiada por nuestros especialistas.</p>
-                        <a href="tel:+123456789" class="btn btn-success btn-sm">Llamar</a>
+                        <a href="tel:831-6100" class="btn btn-success btn-sm">Llamar</a>
                     </div>
                 </div>
             </div>
