e5927fadcac591c2a361ada072994f0d709a0bdd
Rafael Gasperi <rafgasperi@yahoo.com>
2026-04-09 11:52:56 -0500

    bug fix

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -36,6 +36,7 @@ Route::prefix('whatsapp')->name('webhooks.whatsapp.')->group(function () {
     Route::post('/', [WhatsAppWebhookController::class, 'handle'])->name('handle');
 });
 
+
 Route::get('/subscriptions/payments/yappy-ipn', [SuscriptionPaymentController::class, 'yappyIPN'])->name('suscriptions.payments.yappy_ipn');
 
 // HemoScreen API (Integrated & Standalone)
