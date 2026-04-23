6745c9d69a8d3478833d3bf89060a8dbef4913a4
ag-tenorio <atenorio@hcaglobal.com>
2026-04-23 15:55:02 -0500

    bugs fixed delete appointment butto in new appointment modal

--- resources/views/livewire/appointment/modal-save.blade.php
+++ resources/views/livewire/appointment/modal-save.blade.php
@@ -121,7 +121,7 @@
                         @endif
                         <button type="button" wire:click="closeModal" class="btn btn-secondary">{{__('generic.cancel')}}</button>
                         @if(auth()->user()->can('cancelled',$appointment))
-                            <button type="button" wire:click="deleteAppointment({{ $appointment->id }})" class="btn" style="background: #dc3545; color: white;" onclick="return confirm('¿Está seguro de eliminar esta cita?')">{{__('generic.delete')}}</button>
+                            <button type="button" wire:click="deleteAppointment({{ $appointment->id }})" class="btn" style="background: #dc3545; color: white;" wire:confirm="¿Está seguro de eliminar esta cita?">{{__('generic.delete')}}</button>
                         @endif
                     </div>
                 </form>
