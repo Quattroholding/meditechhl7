24d4430eb6f3896d896c172b180005655707a465
Rafael Gasperi <rafgasperi@yahoo.com>
2026-05-19 07:48:52 -0500

    bug fix

---
+++ resources/views/inventories/low-stock.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <div class="page-wrapper">
+        <div class="content">
+            <!-- Page Header -->
+            @component('components.page-header')
+                @slot('title')
+                    Reporte de Stock Bajo
+                @endslot
+                @slot('li_1')
+                    Items con Stock Mínimo
+                @endslot
+            @endcomponent
+            <!-- /Page Header -->
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card card-table show-entire p-2">
+                        <div class="card-body">
+                            <livewire:inventory.low-stock-report/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @component('components.notification-box')
+        @endcomponent
+    </div>
+</x-app-layout>
